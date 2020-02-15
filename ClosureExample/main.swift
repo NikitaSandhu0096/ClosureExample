7f84e47517e0aba3216ffb73dd1d8c29086b8023
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-14 20:29:05 -0500

    Initial Commit

---
+++ ClosureExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClosureExample
+//
+//  Created by Nikita Sandhu on 2020-02-14.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
